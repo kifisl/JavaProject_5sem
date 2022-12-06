4fe2558998ec90320af80570ec984240544d1096
kifisl <nilladarvin@gmail.com>
2022-12-06 21:21:37 +0300

    config

--- src/main/java/spring/security/jwt/bean/dto/Role.java
+++
@@ -1,6 +0,0 @@
-package spring.security.jwt.bean.dto;
-
-public enum  Role {
-    ROLE_ADMIN,
-    ROLE_USER
-}
--- src/main/java/spring/security/jwt/service/MailSender.java
+++
@@ -1,9 +0,0 @@
-package spring.security.jwt.service;
-
-import org.springframework.stereotype.Service;
-import spring.security.jwt.exception.ServiceException;
-
-@Service
-public interface MailSender {
-    void send(String emailTo, String subject, String message)throws ServiceException;
-}
